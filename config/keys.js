fc621f5a0f3934c3cf78ab232155684ced4e77fe
TextError <savinraul1983@yahoo.com>
2021-05-05 18:58:47 +0100

    Connect to DB

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,5 @@
 const PORT = process.env.PORT || 3000;
+const USER = process.env.USER;
+const PASSWORD = process.env.PASSWORD;
 
-module.exports = { PORT };
+module.exports = { PORT, USER, PASSWORD };
--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const { PORT } = require("./config/keys");
+const { PORT, USER, PASSWORD } = require("./config/keys");
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose.connect(`mongodb://${USER}:${PASSWORD}@mongo:27017/?authSource=admin"`)
+  .then(() => console.log("successfully connected to DB!"))
+  .catch(e => console.log(e))
 
 app.get('/', (req, res) => {
   res.send("<h2>Hi there!</h2>")
